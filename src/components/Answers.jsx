5cad956934e7404e14157de28b9d8865f852cfe3
seetaramoruganti <orugantiseetaram@gmail.com>
2024-12-13 02:44:40 -0500

    updated answers component

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -7,27 +7,36 @@ export default function Answers({
   onSelect,
 }) {
   const shuffledAnswers = useRef();
+
   if (!shuffledAnswers.current) {
     shuffledAnswers.current = [...answers];
     shuffledAnswers.current.sort(() => Math.random() - 0.5);
   }
+
   return (
     <ul id="answers">
       {shuffledAnswers.current.map((answer) => {
-        let cssClasses = "";
-        let isSelected = selectedAnswer === answer;
+        const isSelected = selectedAnswer === answer;
+        let cssClass = "";
+
         if (answerState === "answered" && isSelected) {
-          cssClasses = "selected";
+          cssClass = "selected";
         }
+
         if (
           (answerState === "correct" || answerState === "wrong") &&
           isSelected
         ) {
-          cssClasses = answerState;
+          cssClass = answerState;
         }
+
         return (
           <li key={answer} className="answer">
-            <button onClick={() => onSelect(answer)} className={cssClasses}>
+            <button
+              onClick={() => onSelect(answer)}
+              className={cssClass}
+              disabled={answerState !== ""}
+            >
               {answer}
             </button>
           </li>
