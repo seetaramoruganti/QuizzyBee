9e4aa49112736795e846a6d5da9fdc623389c1d0
seetaramoruganti <orugantiseetaram@gmail.com>
2024-12-13 02:45:29 -0500

    updated

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,32 +1,34 @@
 import { useState, useEffect } from "react";
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
     console.log("SETTING TIMEOUT");
     const timer = setTimeout(onTimeout, timeout);
+
     return () => {
       clearTimeout(timer);
     };
   }, [timeout, onTimeout]);
-
-  //   useEffect(() => {
-  //     // This interval is being executed twice recognized with progress bar completing too fast i.e.. 5 sec where choosen was 10 sec because strictmode is running which executes any component twice which shoes a bug
-  //     console.log("SETTING INTERVAL");
-  //     setInterval(() => {          Because we are not cleaning up the timer
-  //       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
-  //     }, 100);
-  //   }, []);
 
   useEffect(() => {
     console.log("SETTING INTERVAL");
     const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
     return () => {
       clearInterval(interval);
     };
   }, []);
-  return <progress id="question-time" max={timeout} value={remainingTime} />;
+
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
 }
